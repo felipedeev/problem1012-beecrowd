3b8dd0dac99fd07dee6073d93a4534de064e81cd
felipedeev <feraichert@gmail.com>
2024-01-03 17:27:34 -0300

    code

---
+++ 1012.js
@@ -0,0 +1,16 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+
+let [A, B, C] = input.split(" ").map(item => parseFloat(item));
+let pi = 3.14159;
+
+let trianguloRetangulo = (A * C) / 2;
+let circulo = pi * Math.pow(C, 2);
+let trapezio = (A + B) * C / 2;
+let quadrado = B * B;
+let retangulo = A * B;
+
+console.log('TRIANGULO: ' + trianguloRetangulo.toFixed(3));
+console.log('CIRCULO: ' + circulo.toFixed(3));
+console.log('TRAPEZIO: ' + trapezio.toFixed(3));
+console.log('QUADRADO: ' + quadrado.toFixed(3));
+console.log('RETANGULO: ' + retangulo.toFixed(3));
